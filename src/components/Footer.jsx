deae5b3a8a195816e83d5e1769e000c124163f06
HanneStray <hanne_stray@hotmail.com>
2025-12-21 22:18:08 +0100

    Removed unsued context and hooks folders, replaced class with className in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer class="bg-white mt-auto border-t text-center py-4 text-sm text-gray-500">
+    <footer className="bg-white mt-auto border-t text-center py-4 text-sm text-gray-500">
       &copy; 2025 Semester Project 2
     </footer>
   );
